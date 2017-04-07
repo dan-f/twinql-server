b8deff19fb6906a0fb9a5673bd00914e2d7776b1
Daniel Friedman <dfriedman58@gmail.com>
2017-04-06 21:06:06 -0400

    Add config for hostname, reverse proxy, better default port

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
-// TODO:
-// - logging?
-// - http or https depending on environment variables
-// - hostname
-// - see https://expressjs.com/en/guide/behind-proxies.html
-// - see https://expressjs.com/en/advanced/best-practice-security.html
+/**
+ * Configure with the following environment variables:
+ * - HOSTNAME (default null) - the hostname of this server; rejects requests for
+ *   different hostnames
+ * - PORT (default 8000) - the port to bind to
+ * - REVERSE_PROXY_IP (default 'loopback') - the IP address(es) of reverse
+ *   proxies to trust.  Accepts values specified here:
+ *   https://expressjs.com/en/guide/behind-proxies.html
+ * - FORWARD_PROXY_URI - The URI of the forward-facing Solid proxy
+ */
 
 const bodyParser = require('body-parser')
 const express = require('express')
 const twinql = require('twinql')
 
-let { PORT, PROXY_URI } = process.env
-PORT = PORT || 80
-PROXY_URI = PROXY_URI || ''
+let { HOSTNAME, PORT, FORWARD_PROXY_URI, REVERSE_PROXY_IP } = process.env
+HOSTNAME = HOSTNAME || null
+PORT = PORT || 8000
+FORWARD_PROXY_URI = FORWARD_PROXY_URI || ''
+REVERSE_PROXY_IP = REVERSE_PROXY_IP || 'loopback'
 
 const app = express()
+
+app.disable('x-powered-by')
+app.set('trust proxy', REVERSE_PROXY_IP)
 
 app.use(bodyParser.text())
 
 app.post('/', (req, res, next) => {
-  const backend = new twinql.LdpBackend({ proxyUri: PROXY_URI })
+  const backend = new twinql.LdpBackend({ proxyUri: FORWARD_PROXY_URI })
   res.set('access-control-allow-origin', '*')
   res.set('vary', 'origin')
   return twinql.query(backend, req.body)
@@ -31,6 +40,12 @@ app.use((err, req, res, next) => {
   res.status(400).json({ '@error': err })
 })
 
-app.listen(PORT, () => {
-  console.log(`...twinql server listening on port ${PORT}`)
+app.listen(PORT, HOSTNAME, () => {
+  console.log(
+    '...twinql server listening on ' + (
+      HOSTNAME
+        ? `<http://${HOSTNAME}:${PORT}>`
+        : `port ${PORT}`
+    )
+  )
 })
